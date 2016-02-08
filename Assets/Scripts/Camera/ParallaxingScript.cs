ce23b8663c69171412361d415473344aa5c8da06
Jacob Peltola <jtpup0@aim.com>
2016-02-08 15:07:40 -0500

    Parallax script/bg and owl anim test

---
+++ Assets/Scripts/Camera/ParallaxingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxingScript : MonoBehaviour {
+
+    public Vector2 speed;
+
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x += Random.value;
+        mat.mainTextureOffset = offset;
+    }
+
+    void Update()
+    {
+        Vector2 offset = mat.mainTextureOffset;
+        offset += Time.deltaTime * speed;
+        mat.mainTextureOffset = offset;
+    }
+}
