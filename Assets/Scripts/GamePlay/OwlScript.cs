ce45b7ac7d842f6ea0bc6541fbc02b1f804e09ec
Jacob Peltola <jtpup0@aim.com>
2016-02-20 11:18:37 -0500

    Basic Owl Movement and Targeting

---
+++ Assets/Scripts/GamePlay/OwlScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OwlScript : MonoBehaviour {
+    public Transform owlTarget;
+    public float owlSpeed;
+
+    public GameObject[] planetTargets;
+
+    private float stuckTimer;
+    private Vector3 previousPos;
+
+    void Start () {
+        planetTargets = GameObject.FindGameObjectsWithTag("planet");
+        FindNewTarget();
+    }
+
+	void Update () {
+        Vector3 direction = owlTarget.position - gameObject.transform.position;
+        direction.Normalize();
+        GetComponent<Rigidbody2D>().velocity = direction * owlSpeed;
+        
+        Debug.Log(GetComponent<Rigidbody2D>().velocity.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        previousPos = gameObject.transform.position;
+
+    }
+
+    public void FindNewTarget()
+    {
+        owlTarget = planetTargets[Random.Range(0, planetTargets.Length)].transform;
+    }
+
+    void Unsticker()
+    {
+        //really bad way to detect if stuck
+        if (this.transform.position == previousPos)
+        {
+            stuckTimer += Time.deltaTime;
+            if (stuckTimer > 1.5f)
+            {
+                FindNewTarget();
+                stuckTimer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GamePlay/tempOwlSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tempOwlSpawner : MonoBehaviour {
+    public float spawnTimer;
+    public GameObject owl;
+    int spawned;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer > 10 && spawned <= 10)
+        {
+            spawned++;
+            Instantiate(owl, transform.position, transform.rotation);
+            spawnTimer = 0;
+        }
+            
+	}
+}
