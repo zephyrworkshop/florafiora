32b83486befd335693831ab1912fc9485c611e4c
Jacob Peltola <jtpup0@aim.com>
2016-02-25 00:25:49 -0500

    MainMenu's "Start" Redirects to JacobWorkScene, added scissors and seed UI

--- Assets/Scripts/Generators/CloudGenerator.cs
+++ Assets/Scripts/Generators/CloudGenerator.cs
@@ -11,7 +11,7 @@ public class CloudGenerator : MonoBehaviour {
         {
             GameObject clone;
             clone = Instantiate(clouds[Random.Range(0, 7)], transform.position, transform.rotation) as GameObject;
-            clone.transform.position += new Vector3(0, Random.Range(-25, 25));
+            clone.transform.position += new Vector3(0, Random.Range(-100, 100));
             clone.GetComponent<Rigidbody2D>().velocity = -transform.right * Random.Range(1.55f, 4.5f);
             Destroy(clone.gameObject, 30);
             timer = Random.Range(0, 2.5f);
