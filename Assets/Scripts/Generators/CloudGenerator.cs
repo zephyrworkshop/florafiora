be2f24fe70c03047da0ce4c603e42d943de3111d
Jacob Peltola <jtpup0@aim.com>
2016-02-25 09:34:24 -0500

    Clouds procedural randomize and generate overtime

--- Assets/Scripts/Generators/CloudGenerator.cs
+++ Assets/Scripts/Generators/CloudGenerator.cs
@@ -7,14 +7,14 @@ public class CloudGenerator : MonoBehaviour {
 
 	void Update () {
         timer += Time.deltaTime;
-        if (timer >= 5)
+        if (timer >= 12)
         {
             GameObject clone;
             clone = Instantiate(clouds[Random.Range(0, 7)], transform.position, transform.rotation) as GameObject;
-            clone.transform.position += new Vector3(0, Random.Range(-100, 100));
+            clone.transform.position += new Vector3(0, Random.Range(-150, 150));
             clone.GetComponent<Rigidbody2D>().velocity = -transform.right * Random.Range(1.55f, 4.5f);
-            Destroy(clone.gameObject, 30);
-            timer = Random.Range(0, 2.5f);
+            Destroy(clone.gameObject, 365);
+            timer = Random.Range(0, 3.5f);
         }
 	}
 }
