4e5e6045d30c0226fa0fef8da9b0acfd22f9520a
Jacob Peltola <jtpup0@aim.com>
2016-02-24 23:07:02 -0500

    Planets art sorts correctly, mainmenu and cloud generator

---
+++ Assets/Scripts/Generators/CloudGenerator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudGenerator : MonoBehaviour {
+    public GameObject[] clouds;
+    public float timer;
+
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= 5)
+        {
+            GameObject clone;
+            clone = Instantiate(clouds[Random.Range(0, 7)], transform.position, transform.rotation) as GameObject;
+            clone.transform.position += new Vector3(0, Random.Range(-25, 25));
+            clone.GetComponent<Rigidbody2D>().velocity = -transform.right * Random.Range(1.55f, 4.5f);
+            Destroy(clone.gameObject, 30);
+            timer = Random.Range(0, 2.5f);
+        }
+	}
+}
