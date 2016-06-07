3dbe9eb1de652fb1deedb6f55d94918b807356c3
Jacob Peltola <jacobpeltola@gmail.com>
2016-06-06 20:53:09 -0400

    Into Pitch revisions: cross fading and music loop

---
+++ Assets/Scripts/Canvas/FadeScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeScript : MonoBehaviour {
+
+    public float timer;
+
+    public bool fadingIn;
+    public bool fadingOut;
+
+    public bool fadeComplete;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (fadingIn && !fadeComplete)
+        {
+            timer += Time.deltaTime;
+            GetComponent<Image>().color = new Color(1, 1, 1, timer / 3);
+            if (timer >= 5)
+            {
+                fadeComplete = true;
+                timer = 5;
+            }
+        }
+        if (fadingOut && !fadeComplete)
+        {
+            timer -= Time.deltaTime;
+            GetComponent<Image>().color = new Color(1, 1, 1, timer / 3);
+            if (timer <= 0 )
+            {
+                fadeComplete = true;
+                timer = 0;
+            }
+        }
+    }
+
+    public void FadeIn()
+    {
+        fadeComplete = false;
+        fadingOut = false;
+        fadingIn = true;
+
+    }
+    public void FadeOut()
+    {
+        fadeComplete = false;
+        fadingIn = false;
+        fadingOut = true;
+    }
+
+}
--- Assets/Scripts/Canvas/PitchScreen.cs
+++ Assets/Scripts/Canvas/PitchScreen.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 
 public class PitchScreen : MonoBehaviour {
+
+    public AudioSource PitchLoop;
 
 	public Image[] splashImages;
 	//public SpriteRenderer[] splashSprites;
@@ -13,49 +16,58 @@ public class PitchScreen : MonoBehaviour {
 
 	private int currentPage = 0;
 
-	// Use this for initialization
 	void Start () {
 		nextImage.enabled = false;
 		SetupSprites();
 	}
+
+    void Update()
+    {
+        //detects if pitch intro is complete and begins loading scene
+        if (currentPage >= 3 && splashImages[currentPage].GetComponent<FadeScript>().fadeComplete)
+        {
+            StartGame();
+        }
+    }
+
 
 	public void SetupSprites()
 	{
 		//Hide all of the pages at the start.
 		foreach (Image page in splashImages)
 		{
-			page.enabled = false;
+            page.color = new Color(1, 1, 1, 0);
+            page.enabled = false;
+            
 		}
-		//Show the starting page.
-		//splashImages[currentPage].enabled = true;
-	}
 
+    }
 	public void firstPage()
 	{
 		nextImage.enabled = true;
 		splashImages[currentPage].enabled = true;
-	}
+        PitchLoop.GetComponent<AudioSource>().Play();
+        splashImages[currentPage].GetComponent<FadeScript>().FadeIn();
+    }
 
-	public void NextPage()
-	{
-
-		if (currentPage < splashImages.Length - 1)
-		{
-			splashImages[currentPage].enabled = false;
-			splashImages[++currentPage].enabled = true;
-		}
-		else
-		{
-			StartGame ();
-		}
-
-	}
+    public void NextPage()
+    {
+        //intercahnges image alphas for a fade effect
+        if (currentPage < splashImages.Length - 1)
+        {
+            splashImages[++currentPage].enabled = true;
+            splashImages[currentPage].GetComponent<FadeScript>().FadeIn();
+            if (splashImages[currentPage].GetComponent<FadeScript>().fadeComplete)
+            {
+                splashImages[currentPage].GetComponent<FadeScript>().FadeOut();
+            }
+        }
+        
+    }
 		
-
 	public void StartGame()
 	{
-		Application.LoadLevel("JacobWorkScene");
+		SceneManager.LoadScene("JacobWorkScene");
 	}
-
 }
 
