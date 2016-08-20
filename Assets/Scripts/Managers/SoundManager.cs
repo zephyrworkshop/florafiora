d7bd0ea64d9bf2f98c34b6f95452b8aa84d09987
Jacob Peltola <jacobpeltola@gmail.com>
2016-08-20 11:08:10 -0400

    UI & SoundManager

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioSource sfxSource;               
+        public AudioSource musicSource;             
+        public static SoundManager instance = null; 
+        public float lowPitchRange = .95f;          
+        public float highPitchRange = 1.05f;        
+
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void PlaySingle(AudioClip clip)
+        {
+            sfxSource.clip = clip;
+            sfxSource.Play();
+        }
+
+        public void RandomizeSfx(params AudioClip[] clips)
+        {
+            int randomIndex = Random.Range(0, clips.Length);
+
+            float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+            sfxSource.pitch = randomPitch;
+            sfxSource.clip = clips[randomIndex];
+            sfxSource.Play();
+        }
+    }
+}
